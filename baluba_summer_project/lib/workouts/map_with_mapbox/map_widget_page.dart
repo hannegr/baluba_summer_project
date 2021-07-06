2b2e349d8c08282480710e96b170d4091e162d1c
Hanne-Grete <14hhannegrete@gmail.com>
2021-07-06 20:02:09 +0200

    added places search and fixed circle on new location

--- baluba_summer_project/lib/workouts/map_with_mapbox/map_widget_page.dart
+++ baluba_summer_project/lib/workouts/map_with_mapbox/map_widget_page.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:mapbox_search_flutter/mapbox_search_flutter.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:color/color.dart';
 
 import 'package:flutter/cupertino.dart';
@@ -42,8 +44,8 @@ class MapWidgetPage extends StatelessWidget {
           } else {
             return MapboxMap(
               zoomGesturesEnabled: true,
-              minMaxZoomPreference: MinMaxZoomPreference(5, 500),
-              accessToken: snapshot.data!['mapbox_api_token'],
+              //minMaxZoomPreference: MinMaxZoomPreference(5, 50),
+              accessToken: snapshot.data!['mapbox_api_token'], // ApiKey
               //'mapbox_api_token' is the name of the key we are using in our config file.
               initialCameraPosition: CameraPosition(
                 target: LatLng(0, 0),
@@ -58,7 +60,6 @@ class MapWidgetPage extends StatelessWidget {
                 );
                 globalController = controller;
                 if (animateCameraResult) {
-                  print('works');
                   controller.addCircle(
                     CircleOptions(
                       circleRadius: 7,
@@ -68,6 +69,7 @@ class MapWidgetPage extends StatelessWidget {
                       circleStrokeOpacity: 1,
                       circleStrokeWidth: 2,
                       geometry: LatLng(location!.latitude, location.longitude),
+                      draggable: false,
                     ),
                   );
                 }
@@ -76,26 +78,44 @@ class MapWidgetPage extends StatelessWidget {
                 final animateCameraResult =
                     await globalController!.animateCamera(
                   CameraUpdate.newCameraPosition(
-                    CameraPosition(zoom: 15.0, target: latLng),
+                    CameraPosition(
+                        zoom: 15.0, target: latLng), //LatLng(69.6489, 18.95508)
                   ),
                 );
                 Location coordinates = new Location();
                 coordinates.lat = latLng.latitude;
                 coordinates.lng = latLng.longitude;
-                var address = await reverseGeoCoding.getAddress(coordinates);
+                var place = await reverseGeoCoding.getAddress(coordinates);
                 print(latLng); //riktige koordinater
-                currentAddress = address[0].toString();
-                print(address[0]
-                    .toString()); //blir en tom liste hmm ble feil siden jeg tok Country norway XD
+                currentAddress = place[0].placeName;
+                ;
+                print(currentAddress);
+                globalController!.clearCircles();
+                //globalController!.clearSymbols();
+
                 if (animateCameraResult) {
+                  globalController!.addCircle(
+                    CircleOptions(
+                      circleRadius: 7,
+                      circleColor: '#990000',
+                      circleOpacity: 0.5,
+                      circleStrokeColor: '#ff0000',
+                      circleStrokeOpacity: 6,
+                      circleStrokeWidth: 5,
+                      geometry: LatLng(latLng.latitude, latLng.longitude),
+                      draggable: false,
+                    ),
+                  );
+                  /*
                   globalController!.addSymbol(
                     (SymbolOptions(
                       geometry: LatLng(coordinates.lat, coordinates.lng),
                       iconImage: "assets\\images\\location_pin.png",
                       iconSize: 30,
                     )),
-                  );
+                  );*/
                 }
+                /*
 
                 staticImage.getStaticUrlWithMarker(
                   center: coordinates,
@@ -110,6 +130,7 @@ class MapWidgetPage extends StatelessWidget {
                   render2x: true,
                 );
                 staticImage == null ? print('error!') : print('yes');
+              */
               },
             );
           }
